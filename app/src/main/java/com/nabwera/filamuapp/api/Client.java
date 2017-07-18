f7845d8125c63e595107fda62b34e579066638d1
Roger Taracha <rojtaracha@gmail.com>
2017-07-18 18:23:21 +0300

    Define API Client and Service: + Creating the API Client & Service to be able to load JSON data from.

--- app/src/main/java/com/nabwera/filamuapp/api/Client.java
+++ app/src/main/java/com/nabwera/filamuapp/api/Client.java
@@ -1,8 +1,24 @@
 package com.nabwera.filamuapp.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by nabwera on 18/07/2017.
  */
 
 public class Client {
+
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    public static Retrofit retrofit = null;
+
+    public static Retrofit getClient(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
--- app/src/main/java/com/nabwera/filamuapp/api/Service.java
+++ app/src/main/java/com/nabwera/filamuapp/api/Service.java
@@ -1,8 +1,20 @@
 package com.nabwera.filamuapp.api;
 
+import com.nabwera.filamuapp.model.MoviesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by nabwera on 18/07/2017.
  */
 
-public class Service {
+public interface Service {
+
+    @GET("movie/popular/")
+    Call<MoviesResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/top_rated/")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
 }
