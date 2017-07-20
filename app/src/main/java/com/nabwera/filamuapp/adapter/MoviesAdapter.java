7737400ed41500dc405b28dd02a1e36c064417d4
Roger Taracha <rojtaracha@gmail.com>
2017-07-20 08:54:09 +0300

    Implementing MoviesAdapter: + Handling onClick listeners on every card in the grid layout of the RecyclerView. + Passing Extras into the Intent to another Activity [Detail Activity]. + JSON API calls with image caching were made seamlessly easy using Retrofit and Glide.

--- app/src/main/java/com/nabwera/filamuapp/adapter/MoviesAdapter.java
+++ app/src/main/java/com/nabwera/filamuapp/adapter/MoviesAdapter.java
@@ -1,8 +1,94 @@
 package com.nabwera.filamuapp.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.nabwera.filamuapp.DetailActivity;
+import com.nabwera.filamuapp.R;
+import com.nabwera.filamuapp.model.Movie;
+
+import java.util.List;
+
 /**
  * Created by nabwera on 18/07/2017.
  */
 
-public class MoviesAdapter {
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Movie> movieList;
+
+    public MoviesAdapter(Context mContext, List<Movie> movieList){
+        this.mContext = mContext;
+        this.movieList = movieList;
+    }
+
+    // setting bind data to our views
+    @Override
+    public MoviesAdapter.MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.movie_card, ViewGroup, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final MoviesAdapter.MyViewHolder viewHolder, int i){
+        viewHolder.title.setText(movieList.get(i).getOriginalTitle());
+        String vote = Double.toString(movieList.get(i).getVoteAverage());
+        viewHolder.userrating.setText(vote);
+
+        // Using Glide to load the thumbnail
+        Glide.with(mContext)
+                .load(movieList.get(i).getPosterPath())
+                .placeholder(R.drawable.load)
+                .into(viewHolder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount(){
+        return movieList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView title, userrating;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view){
+            super(view);
+            // TIP: You can use butterknife here to simply field binding for views.
+            title = (TextView) view.findViewById(R.id.title);
+            userrating = (TextView) view.findViewById(R.id.userrating);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+
+            // Handle an onClick listener on every card in the grid layout of the RecyclerView.
+            view.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION){
+                        Movie clickedDataItem = movieList.get(pos);
+                        Intent intent = new Intent(mContext, DetailActivity.class);
+                        intent.putExtra("original_title", movieList.get(pos).getOriginalTitle());
+                        intent.putExtra("poster_path", movieList.get(pos).getPosterPath());
+                        intent.putExtra("overview", movieList.get(pos).getOverview());
+                        intent.putExtra("vote_average", Double.toString(movieList.get(pos).getVoteAverage()));
+                        intent.putExtra("release_date", movieList.get(pos).getReleaseDate());
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        mContext.startActivity(intent);
+                        Toast.makeText(v.getContext(), "You clicked " + clickedDataItem.getOriginalTitle(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
 }
