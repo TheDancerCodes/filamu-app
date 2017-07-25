b2f62328a8be2d29c2c638dd9e6c4fcf44dc5a61
Roger Taracha <rojtaracha@gmail.com>
2017-07-25 18:16:18 +0300

    Implement Material Favorite Button: [+] Import and Implement the Material Fav Button in content_detail.xml [+] Initialize the Material Favorite Button in DetailActivity.

--- app/src/main/java/com/nabwera/filamuapp/DetailActivity.java
+++ app/src/main/java/com/nabwera/filamuapp/DetailActivity.java
@@ -1,9 +1,12 @@
 package com.nabwera.filamuapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.github.ivbaranov.mfb.MaterialFavoriteButton;
 import com.nabwera.filamuapp.adapter.TrailerAdapter;
 import com.nabwera.filamuapp.api.Client;
 import com.nabwera.filamuapp.api.Service;
@@ -85,6 +89,37 @@ public class DetailActivity extends AppCompatActivity {
             Toast.makeText(this, "No API Data", Toast.LENGTH_SHORT).show();
         }
 
+        // Declare the MaterialFavorite Button
+        MaterialFavoriteButton materialFavoriteButtonNice =
+                (MaterialFavoriteButton) findViewById(R.id.favorite_button);
+
+        // Call SharedPreferences to save the state
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        materialFavoriteButtonNice.setOnFavoriteChangeListener(
+                new MaterialFavoriteButton.OnFavoriteChangeListener(){
+                    @Override
+                    public void onFavoriteChanged(MaterialFavoriteButton buttonView, boolean favorite){
+                        if (favorite){
+                            SharedPreferences.Editor editor = getSharedPreferences("com.nabwera.filamuapp.DetailActivity", MODE_PRIVATE).edit();
+                            editor.putBoolean("Favorite Added", true);
+                            editor.commit();
+
+                            // method that saves metadata of the movie to the sqlite db
+                            saveFavorite();
+                            Snackbar.make(buttonView, "Added to Favorite",
+                                    Snackbar.LENGTH_SHORT).show();
+                        }else{
+                            SharedPreferences.Editor editor = getSharedPreferences("com.nabwera.filamuapp.DetailActivity", MODE_PRIVATE).edit();
+                            editor.putBoolean("Favorite Removed", true);
+                            editor.commit();
+                            Snackbar.make(buttonView, "Removed from Favorite",
+                                    Snackbar.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
         // The initViews method makes a handshake with the loadJSON
         initViews();
 
@@ -131,6 +166,7 @@ public class DetailActivity extends AppCompatActivity {
 
     }
 
+    // A JSON Call for the Trailer
     private void loadJSON(){
         int movie_id = getIntent().getExtras().getInt("id");
 
