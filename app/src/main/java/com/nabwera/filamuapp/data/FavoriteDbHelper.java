f2971bf795defc74f52db3d78978620ba3133f20
Roger Taracha <rojtaracha@gmail.com>
2017-09-26 16:25:29 +0300

    [FEATURE] Add methods for the CRUD oerations with DB: [+] Using Cursor to iterate through table entries. [+] Sorting the order in Ascending order.

--- app/src/main/java/com/nabwera/filamuapp/data/FavoriteDbHelper.java
+++ app/src/main/java/com/nabwera/filamuapp/data/FavoriteDbHelper.java
@@ -1,9 +1,17 @@
 package com.nabwera.filamuapp.data;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import com.nabwera.filamuapp.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by nabwera on 25/07/2017.
@@ -54,5 +62,84 @@ public class FavoriteDbHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        db.execSQL("DROP TABLE IF EXISTS " + FavoriteContract.FavoriteEntry.TABLE_NAME);
+        onCreate(db);
     }
+
+    /**
+     * Methods for the CRUD operations
+     */
+
+    // Insert the data of the Movie iinto the corresponding columns in the DB
+    public void addFavorite(Movie movie){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(FavoriteContract.FavoriteEntry.COLUMN_MOVIEID, movie.getId());
+        values.put(FavoriteContract.FavoriteEntry.COLUMN_TITLE, movie.getOriginalTitle());
+        values.put(FavoriteContract.FavoriteEntry.COLUMN_USERRATING, movie.getVoteAverage());
+        values.put(FavoriteContract.FavoriteEntry.COLUMN_POSTER_PATH, movie.getPosterPath());
+        values.put(FavoriteContract.FavoriteEntry.COLUMN_PLOT_SYNOPSIS, movie.getOverview());
+        db.insert(FavoriteContract.FavoriteEntry.TABLE_NAME, null, values);
+        db.close();
+    }
+
+    // To delete a particular entry
+    public void deleteFavorite(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.delete(FavoriteContract.FavoriteEntry.TABLE_NAME, FavoriteContract.FavoriteEntry.COLUMN_MOVIEID+ "=" + id, null);
+    }
+
+    // To fetch all movie details
+    public List<Movie> getAllFavorite(){
+        String[] columns = {
+                FavoriteContract.FavoriteEntry._ID,
+                FavoriteContract.FavoriteEntry.COLUMN_MOVIEID,
+                FavoriteContract.FavoriteEntry.COLUMN_TITLE,
+                FavoriteContract.FavoriteEntry.COLUMN_USERRATING,
+                FavoriteContract.FavoriteEntry.COLUMN_POSTER_PATH,
+                FavoriteContract.FavoriteEntry.COLUMN_PLOT_SYNOPSIS
+        };
+
+        // Sorting the order
+        String sortOrder =
+                FavoriteContract.FavoriteEntry._ID + " ASC";
+
+        // Populating the List
+        List<Movie> favoriteList = new ArrayList<>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // Cursor acts as an iterator
+        Cursor cursor = db.query(FavoriteContract.FavoriteEntry.TABLE_NAME,
+                columns,
+                null, // 1st null - WHERE clause
+                null, // 2nd null - value for the WHERE clause
+                null, // 3rd null - group by the rows
+                null, // 4th null - filter by each row group
+                sortOrder);
+
+        // Traverse through all rows and add to the list
+        // NB: All data is coming as a string & hence the need to convert to approriate data type
+        if (cursor.moveToFirst()){
+            do {
+                Movie movie = new Movie();
+                movie.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex(FavoriteContract.FavoriteEntry.COLUMN_MOVIEID))));
+                movie.setOriginalTitle(cursor.getString(cursor.getColumnIndex(FavoriteContract.FavoriteEntry.COLUMN_TITLE)));
+                movie.setVoteAverage(Double.parseDouble(cursor.getString(cursor.getColumnIndex(FavoriteContract.FavoriteEntry.COLUMN_USERRATING))));
+                movie.setPosterPath(cursor.getString(cursor.getColumnIndex(FavoriteContract.FavoriteEntry.COLUMN_POSTER_PATH)));
+                movie.setOverview(cursor.getString(cursor.getColumnIndex(FavoriteContract.FavoriteEntry.COLUMN_PLOT_SYNOPSIS)));
+
+                // Add the user record to the List
+                favoriteList.add(movie);
+
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+
+        return favoriteList;
+    }
+
 }
